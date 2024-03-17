4e58740fe996920a0448ed13dc9264a35daea430
Wahid Khan <wahidzk0091@gmail.com>
2024-03-17 16:58:15 +0000

    Pissarro: debloat certain apps
    Signed-off-by: Wahid Khan <wahidzk0091@gmail.com>

--- device.mk
+++ device.mk
@@ -103,6 +103,10 @@ PRODUCT_COPY_FILES += \
     system/core/libprocessgroup/profiles/cgroups_30.json:$(TARGET_COPY_OUT_VENDOR)/etc/cgroups.json \
     $(LOCAL_PATH)/configs/task_profiles.json:$(TARGET_COPY_OUT_VENDOR)/etc/task_profiles.json
 
+# Debloat
+PRODUCT_PACKAGES += \
+    Debloat
+
 # Display
 PRODUCT_PACKAGES += \
     android.hardware.graphics.allocator@4.0.vendor \
